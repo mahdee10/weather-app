214ceb580bcee047c0368531a4d6595c5acab4d8
mahdee10 <diranimahdi2002@gmail.com>
2023-11-09 14:08:22 +0100

    sd

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import SideBar from './components/sideBar';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cities from './pages/Cities';
-import { useState } from 'react';
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  function c(){
-    setIsMenuOpen(!isMenuOpen);
-  }
+  
   return (
-    <div className="flex sm:justify-between p-5 bg-main h-screen ">
-      <div className={`sm:w-side sm:bg-gb sm:h-full  rounded-3xl ${isMenuOpen ? "bg-gb h-full w-full " : ""}`}>
-        <SideBar c={c}></SideBar>
+    <div className="flex sm:flex-row flex-col sm:justify-between p-5 bg-main h-screen ">
+      <div className='sm:w-side sm:bg-gb sm:h-full w-full rounded-3xl'>
+        <SideBar></SideBar>
       </div>
-      <div className={`sm:w-cont ${isMenuOpen ? "" : "sidebar-closed"}`}>
+      <div className='sm:w-cont '>
         <BrowserRouter>
           <Routes>
             <Route path="/" >
