5beecaccb62de572bcfc69a4cb49142d89dc3380
mahdee10 <diranimahdi2002@gmail.com>
2023-11-23 14:24:15 +0100

    ad

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,91 +1,15 @@
 import { useCityContext } from "../context/cityContext";
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-
 import axios from 'axios';
 import gear3 from "../assets/imgs/gear3.png"
 import gear2 from "../assets/imgs/gear2.png"
 import WeatherImage from "../components/weatherImage";
 import TodayForecast from "../components/TodayForecast";
 export default function Weather() {
-    // const todayForecast = [
-    //     {
-    //         0:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "11000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         0:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         2:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         3:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         4:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         5:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         6:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         7:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         8:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         9:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         10:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     },
-    //     {
-    //         11:{time: "2023-11-23T11:00:00Z",
-    //         values:{weatherCode: "10000",
-    //         temp: "25"}
-    //         }
-    //     }
-
-    // ]
     const [weatherData, setWeatherData] = useState({});
     const [todayForecast, setForecastData] = useState({});
-    const apiKey = 'Lryk5QBR1xtWHKoymkxnzasaLjJSnNnt';
+    const apiKey = 'Pd6k0YnQLWeDVgkGKmoG43wyBXDZBtdj';
 
     const { city } = useCityContext();
     useEffect(() => {
@@ -93,7 +17,7 @@ export default function Weather() {
             .get(`https://api.tomorrow.io/v4/weather/realtime?location=${city}&apikey=${apiKey}`)
             .then((response) => {
                 setWeatherData(response.data);
-                console.log(weatherData)
+                // console.log(weatherData)
             })
             .catch((error) => {
                 console.error('Error fetching weather data:', error);
@@ -104,9 +28,8 @@ export default function Weather() {
         axios
             .get(`https://api.tomorrow.io/v4/weather/forecast?location=${city}&apikey=${apiKey}`)
             .then((response) => {
-                setForecastData(response.data);
-                console.log(response.data,"hello")
-                console.log(todayForecast.timelines.hourly,"klb")
+                setForecastData(response.data.timelines);
+                // console.log(todayForecast)
             })
             .catch((error) => {
                 console.error('Error fetching forecast data:', error);
@@ -121,8 +44,7 @@ export default function Weather() {
     };
     return (
         <div className="">
-            {Object.keys(weatherData).length === 0 || todayForecast.length === 0 ? (
-
+            {Object.keys(weatherData).length === 0 || todayForecast.length === 0 || !todayForecast.hourly ? (
                 <div className="absolute top-2/4 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                     <div className="flex">
                         <img style={rotatingStyle2} src={gear3} alt="gear3"></img>
@@ -155,34 +77,10 @@ export default function Weather() {
                         <WeatherImage imageName={"10010"} isLarge={true}></WeatherImage>
                     </div>
                     <section className="p-5 sm:w-full bg-gb rounded-xl">
-                        <TodayForecast data={todayForecast.timelines.hourly} />
+                        <TodayForecast data={todayForecast.hourly} />
                     </section>
                 </div>
             )}
-            {/* <div className="sm:flex sm:h-full hel">
-                <div className="sm:w-3/5 sm:flex sm:flex-col justify-between sm:full">
-                    <div className="p-10 sm:flex sm:flex-row flex-col sm:justify-between justify-center sm:items-stretch items-center sm:w-full">
-                        <div className="flex flex-col justify-between">
-                            <h1 className="text-white text-center text-3xl font-extrabold">
-                                MADRID
-                            </h1>
-                            <div className="sm:hidden flex justify-center py-2">
-                                <WeatherImage imageName={"10010"} size={"l"}></WeatherImage>
-                            </div>
-                            <h2 className="text-white text-center text-5xl font-extrabold">
-                                31°
-                            </h2>
-                        </div>
-                        <div className="sm:block hidden">
-                            <WeatherImage imageName={"10010"} size={"xl"}></WeatherImage>
-                        </div>
-                    </div>
-
-                    <section className="p-5 sm:w-full bg-gb rounded-xl">
-                        <TodayForecast data={todayForecast} />
-                    </section>
-                </div>
-            </div> */}
 
         </div>
 
