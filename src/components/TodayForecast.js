c2b4fcef425490b25fc6dec0400d17f6df9230cc
mahdee10 <diranimahdi2002@gmail.com>
2023-11-23 12:51:50 +0100

    ad

--- src/components/TodayForecast.js
+++ src/components/TodayForecast.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import 'swiper/css/navigation';
