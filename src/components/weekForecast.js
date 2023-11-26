abfd51145b2b01f28e01ea3ff0806f0b3c5895fa
mahdee10 <diranimahdi2002@gmail.com>
2023-11-26 15:21:46 +0100

    asd

--- src/components/weekForecast.js
+++ src/components/weekForecast.js
@@ -6,14 +6,14 @@ export default function WeekForecast(props) {
     const temperatureMin = Math.floor(props.temperatureMin);
     return (
         <div className="flex justify-between py-3 items-center bg-gb ">
-            <h3 className="text-g min-w-[50px]">{dayOfWeek}</h3>
-            <div className="sm:block hidden">
+            <h3 className="text-g w-[30%]">{dayOfWeek}</h3>
+            <div className="sm:block hidden w-[30%]">
                 <WeatherImage imageName={props.weatherCodeMax} size={"s"}></WeatherImage>
             </div>
-            <div className="sm:hidden block">
+            <div className="sm:hidden block w-[30%]">
                 <WeatherImage imageName={props.weatherCodeMax} size={"xs"}></WeatherImage>
             </div>
-            <h3 className="text-g"><span className="text-white">{temperatureMax}</span>/{temperatureMin}</h3>
+            <h3 className="text-g w-[30%]"><span className="text-white">{temperatureMax}</span>/{temperatureMin}</h3>
         </div>
     )
 }
