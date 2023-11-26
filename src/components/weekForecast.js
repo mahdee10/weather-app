6ef8ecba39b0dc7c849d4919d13c71808f06512d
mahdee10 <diranimahdi2002@gmail.com>
2023-11-26 14:14:38 +0100

    asd

--- src/components/weekForecast.js
+++ src/components/weekForecast.js
@@ -1,9 +1,10 @@
 import WeatherImage from "./weatherImage";
 
 export default function WeekForecast(props) {
+    const dayOfWeek = new Date(props.time).toLocaleDateString('en-US', { weekday: 'short' });
     return (
         <div className="flex justify-between py-3 items-center bg-gb ">
-            <h3 className="text-g min-w-[40px]">{props.time}</h3>
+            <h3 className="text-g min-w-[40px]">{dayOfWeek}</h3>
             <div className="sm:block hidden">
                 <WeatherImage imageName={props.weatherCodeMax} size={"s"}></WeatherImage>
             </div>
