4ceda415e99903d95fbe93dae425ee9643e387fd
mahdee10 <diranimahdi2002@gmail.com>
2023-12-02 23:53:46 +0100

    ad

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Weather from './pages/Weather';
 import SideBar from './components/sideBar';
 import ReactDOM from "react-dom/client";
-import {Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cities from './pages/Cities';
 import { CityProvider } from './context/cityContext';
 import Settings from './pages/Settings';
@@ -11,24 +11,24 @@ function App() {
 
   return (
     <CityProvider>
+      <BrowserRouter>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:400,500,700&display=swap" />
-      <div className="flex sm:flex-row flex-col sm:justify-between p-5 bg-main h-screen ">
+      <div className="flex sm:flex-row flex-col sm:justify-between p-5 bg-main sm:h-screen ">
         <div className='sm:w-side sm:bg-gb sm:h-full w-full rounded-3xl'>
           <SideBar></SideBar>
         </div>
         <div className='sm:w-cont sm:h-full'>
             <Routes>
-              <Route path="/" >
-                <Route index element={<Weather />} />
+                <Route path='/' element={<Weather />} />
                 <Route path="cities" element={<Cities />} />
                 <Route path="settings" element={<Settings />} />
-              </Route>
             </Routes>
         </div>
       </div>
+      </BrowserRouter>
+
     </CityProvider>
   );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
 export default App;
