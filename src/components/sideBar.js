3493dfda45992eee5a6baedb8b8f2e5f3c736860
mahdee10 <diranimahdi2002@gmail.com>
2023-12-03 00:11:31 +0100

    ad

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -2,6 +2,7 @@ import logo from "../assets/imgs/logo2.png"
 import weather from "../assets/imgs/weather.png"
 import cities from "../assets/imgs/list.png"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 export default function SideBar() {
     const [pathname, setPath] = useState(window.location.pathname)
     useEffect(() => {
@@ -10,25 +11,26 @@ export default function SideBar() {
 
     return (
         // <div className='sm:w-side bg-gb h-full rounded-3xl'>
-        <div className="text-white sm:bg-gb h-full  flex   flex-col w-full rounded-3xl pt-5">
+        <div className="text-white sm:bg-gb sm:h-full  flex   flex-col w-full rounded-3xl pt-5">
             <div className="flex justify-center">
             <img src={logo} alt="logo" className="sm:w-fit sm:h-fit w-10"></img>
 
             </div>
             <div className="flex sm:flex-col flex-row sm:items-center justify-between  sm:pt-20 nav">
-                <a href="/" className={`flex flex-col items-center mt-5 text-g hover:text-white ${pathname === '/' ? 'active' : ''}`}>
+                <Link to="/" className={`flex flex-col items-center mt-5 text-g hover:text-white ${pathname === '/' ? 'active' : ''}`}>
 
                     <img src={weather} alt="logo" className="w-5 h-fit"></img>
                     <p>Weather</p>
-                </a>
-                <a href="/cities" className={`flex flex-col items-center mt-5 text-g hover:text-white ${pathname === '/cities' ? 'active' : ''}`}>
+                </Link>
+                <Link to="/cities" className={`flex flex-col items-center mt-5 text-g hover:text-white ${pathname === '/cities' ? 'active' : ''}`}>
                     <img src={cities} alt="logo" className="w-5 h-fit"></img>
                     <p>Cities</p>
-                </a>
-                <a href="/settings" className={`flex flex-col items-center mt-5 text-g hover:text-white ${pathname === '/settings' ? 'active' : ''}`}>
+                </Link>
+                
+                <Link to={"/settings"} className={`flex flex-col items-center mt-5 text-g hover:text-white ${pathname === '/settings' ? 'active' : ''}`}>
                     <img src={cities} alt="logo" className="w-5 h-fit"></img>
                     <p>Settings</p>
-                </a>
+                </Link>
             </div>
         </div>
         // </div>
